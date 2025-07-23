1da65eaa53dd125901af6efc9030b01fd7fc4df3
bsurajpatra <ankitsuraj1111@gmail.com>
2025-07-23 23:29:29 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className="hello-world-container">
+      Hello, World!
+    </div>
+  )
+}
+
+export default App
